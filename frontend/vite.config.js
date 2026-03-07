b72256f07217050f973e112ec18b65cad93857b0
plesalex100@gmail.com <plesalex100@gmail.com>
2026-03-07 20:10:45 +0200

    build using .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      tailwindcss(),
+      react(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    build: {
+      outDir: env.BUILD_PATH || 'dist',
+    },
+  }
 })
