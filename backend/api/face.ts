598dc832f706befcf31b61f6439e4a79fdf1ab37
plesalex100 <plesalex100@gmail.com>
2026-03-07 14:02:32 +0200

    api enroll

--- backend/api/face.ts
+++ backend/api/face.ts
@@ -3,14 +3,14 @@ import { Router, Request, Response } from "express";
 import { AzureKeyCredential } from "@azure/core-auth";
 import createClient, { FaceClient } from "@azure-rest/ai-vision-face";
 
-const endpoint = process.env.FACE_ENDPOINT;
-const apiKey = process.env.FACE_API_KEY;
-
-if (!endpoint || !apiKey) {
-    throw new Error("Please provide a face endpoint and api key in the .env file");
-}
-
 const createFaceClient = (): FaceClient => {
+    const endpoint = process.env.FACE_ENDPOINT;
+    const apiKey = process.env.FACE_API_KEY;
+
+    if (!endpoint || !apiKey) {
+        throw new Error("Please provide a face endpoint and api key in the .env file");
+    }
+
     const credential = new AzureKeyCredential(apiKey);
     const client = createClient(endpoint, credential);
     return client;
@@ -19,15 +19,21 @@ const createFaceClient = (): FaceClient => {
 const router = Router(); 
 
 router.get("/health", (req: Request, res: Response) => {
-    
     try {
-        const client = createFaceClient();
-        res.send("Face API is healthy");
-
+        const _client = createFaceClient();
+        res.json({success: true, message: "Api is healthy"});
     } catch (error) {
-        res.status(500).send("Face API is not healthy");
+        res.status(500).json({success: false, message: "Face API is not healthy"});
     }
+});
+
+router.post("/enroll", function(req: Request, res: Response){
+    
+    const { name, image } = req.body;
+
 })
+
+
 
 
 export default router;
