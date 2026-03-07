4d4ff70b2102330ed373a7e08718c7b65cbf4ffd
plesalex100 <plesalex100@gmail.com>
2026-03-07 14:12:18 +0200

    typscript should inferr

--- backend/api/face.ts
+++ backend/api/face.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 
 import { AzureKeyCredential } from "@azure/core-auth";
-import createClient, { FaceClient } from "@azure-rest/ai-vision-face";
+import createClient from "@azure-rest/ai-vision-face";
 
-const createFaceClient = (): FaceClient => {
+const createFaceClient = () => {
     const endpoint = process.env.FACE_ENDPOINT;
     const apiKey = process.env.FACE_API_KEY;
 
@@ -16,9 +16,9 @@ const createFaceClient = (): FaceClient => {
     return client;
 }
 
-const router = Router(); 
+const router = Router();
 
-router.get("/health", (req: Request, res: Response) => {
+router.get("/health", (req, res) => {
     try {
         const _client = createFaceClient();
         res.json({success: true, message: "Api is healthy"});
@@ -27,9 +27,17 @@ router.get("/health", (req: Request, res: Response) => {
     }
 });
 
-router.post("/enroll", function(req: Request, res: Response){
+type EnrollRequest = {
+    name: string;
+    image: string;
+}
+
+router.post("/enroll", async function(req, res){
     
-    const { name, image } = req.body;
+    const { name, image } = req.body as EnrollRequest;
+    if (!name || !image) {
+        return res.status(400).json({success: false, message: "Name and image are required"});
+    }
 
 })
 
