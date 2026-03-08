5029f34015afc8afbc904698f2aa65091235aac9
plesalex100@gmail.com <plesalex100@gmail.com>
2026-03-08 12:50:34 +0200

    presentation serving

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import API from "./api/index";
 
@@ -16,6 +22,10 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Frontend here");
+});
+
+app.get("/api/presentation", (req, res) => {
+    res.sendFile(path.join(__dirname, "presentation.pdf"));
 });
 
 app.use("/api", API);
